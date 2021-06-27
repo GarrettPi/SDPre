7de33a7f93c05137d333f0bef439dae7c61b930f
Garrett Pipes <garrett.pipes@gmail.com>
2021-06-26 19:14:33 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Garrett Pipes";
+    int age = 32;
+    String state = "Texas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
