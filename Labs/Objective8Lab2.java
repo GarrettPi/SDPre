8644d910467c8dac38752bc480bf1d559de7834e
Garrett Pipes <garrett.pipes@gmail.com>
2021-06-27 13:26:18 -0500

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    int curSumTotal = 0;
+
+    for(int i = 1; i <= 20; i++){
+      curSumTotal += i;
+    }
+    System.out.println(curSumTotal);
+  }
+}
